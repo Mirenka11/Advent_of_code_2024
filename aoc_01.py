fae5bcd0395c8e5105639edf442a0facbe1200f7
Mirenka11 <kacka@hrnecek.com>
2024-12-21 14:19:47 +0100

    aoc_01: Dokonceni 1. ukolu

--- aoc_01.py
+++ aoc_01.py
@@ -1,3 +1,10 @@
 import pandas as pd 
-data = pd.read_csv("input_data.txt", header=None, sep=" ")
-sorted_data = [sorted(data[0]), sorted(data[1])]
+data = pd.read_csv("input_data.txt", header=None, sep="   ", engine="python")
+sorted_data = list(zip(sorted(data[0]), sorted(data[1])))
+print(sorted_data)
+cumulative_distance = 0
+#print(data)
+for i, j in sorted_data:
+    cumulative_distance += abs(i-j)
+
+print(cumulative_distance)
