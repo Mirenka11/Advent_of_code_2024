3b352a976e4fdaa19966c23d6666eec36367ffdc
Mirenka11 <kacka@hrnecek.com>
2024-12-23 00:06:53 +0100

    day_05: Ukol 1 dne 5 dokoncen

---
+++ Day_05/aoc_day_05_01.py
@@ -0,0 +1,20 @@
+from functools import cmp_to_key
+# ok bylo by fajn pochopit jak tenhle neat trik funguje
+def comparing_function(lower, higher):
+    if (lower, higher) in rules:
+        return -1
+    elif (higher, lower) in rules:
+        return 1
+    else: return 0
+with open('Day_05/input_data.txt') as input_data:
+    rules, pages = input_data.read().split('\n\n')
+
+rules = [(int(i), int(j)) for rule in rules.split() for i, j in rule.split("|")]
+sum_middle_pages = 0
+for page_sequence in pages.split():
+    page_list = page_sequence.split(',')
+    sorted_pages = sorted(page_list, key=cmp_to_key(comparing_function)) # FIXME takhle ne :(
+    if page_list == sorted_pages:
+        sum_middle_pages += int(sorted_pages[len(sorted_pages)//2])
+
+print(sum_middle_pages)
---
+++ Day_05/aoc_day_05_01_v2.py
@@ -0,0 +1,20 @@
+from functools import cmp_to_key
+# splneno s pomoci
+# TODO zkusit to prepsat jinak
+
+with open('Day_05/input_data.txt') as input_data:
+    rules, pages = input_data.read().split('\n\n')
+
+cmp = cmp_to_key(lambda lower, higher: -(lower+'|'+higher in rules)) # tohle je silena cerna magie
+ 
+sum_middle_pages = 0
+for page_sequence in pages.split():
+    page_list = page_sequence.split(',')
+    sorted_pages = sorted(page_list, key=cmp)
+    if page_list == sorted_pages:
+        sum_middle_pages += int(sorted_pages[len(sorted_pages)//2])
+
+print(sum_middle_pages)
+
+
+    
