5a74070ce151046b540efbc125b58e0524d792a4
Mirenka11 <kacka@hrnecek.com>
2024-12-22 19:24:23 +0100

    day_03: Den 3 dokoncen!

--- Day_03/aoc_day_03_02.py
+++ Day_03/aoc_day_03_02.py
@@ -1,22 +1,34 @@
 import re 
+
 def mul(a:int, b:int)-> int:
     return a*b
 
 with open("Day_03/input_data.txt", "r") as input_data:
     corrupted_data = input_data.read()
-#corrupted_data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-do = re.sub(r"don't\(\).*do\(\)", "do()", corrupted_data)
-  
-print(do)
-#print(len(corrupted_data)-len(do))
-print(re.findall("don't", do))
+    corrupted_data = corrupted_data.replace("\n", "")
+instructions = re.sub(r"do\(\)", "\ndo()", corrupted_data)
+instructions = re.sub(r"don't\(\)", "\ndon't()", instructions)
 
-instructions_do = re.findall("mul\([0-9]+,[0-9]+\)", do)
-#print(instructions_do)
+instructions = instructions.split("\n")
 
 
-added_results = 0
+instructions_do = []
+instructions_dont = [] #kontrolni soucet
+for instruction in instructions:
+    print(instruction, end = "\n\n")
+    if re.findall(r"^don't\(\)", instruction):
+        instructions_dont += re.findall(r"mul\(\d{1,3},\d{1,3}\)", instruction)
+    else:
+        instructions_do += re.findall(r"mul\(\d{1,3},\d{1,3}\)", instruction)
+added_results_do = 0
+added_results_dont = 0
 for instruction in instructions_do:
-    added_results += eval(instruction)
-print(added_results)
+    added_results_do += eval(instruction)
+for instruction in instructions_dont:
+    added_results_dont += eval(instruction)
+
+print(f"do: {added_results_do}\n\
+don't: {added_results_dont}\n\
+sum: {added_results_dont+added_results_do}\n\
+check_sum: {170068701 - (added_results_dont+added_results_do)}")
 
