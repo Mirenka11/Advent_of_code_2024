9e26ee6ed44c5712dbd0ef0ca13568450dbdaaee
Mirenka11 <kacka@hrnecek.com>
2024-12-22 16:56:22 +0100

    day_03:Zatim nefunkcni reseni druheho ukolu

--- Day_03/aoc_day_03_02.py
+++ Day_03/aoc_day_03_02.py
@@ -4,8 +4,19 @@ def mul(a:int, b:int)-> int:
 
 with open("Day_03/input_data.txt", "r") as input_data:
     corrupted_data = input_data.read()
+#corrupted_data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+do = re.sub(r"don't\(\).*do\(\)", "do()", corrupted_data)
+  
+print(do)
+#print(len(corrupted_data)-len(do))
+print(re.findall("don't", do))
 
-instructions_do = re.findall("do\(\)*mul\([0-9]+,[0-9]+\)", corrupted_data) # tohle nefunguje :( 
-instructions_dont = re.findall("don't\(\)*mul\([0-9]+,[0-9]+\)", corrupted_data)
-print(instructions_do)
-print(instructions_dont) 
+instructions_do = re.findall("mul\([0-9]+,[0-9]+\)", do)
+#print(instructions_do)
+
+
+added_results = 0
+for instruction in instructions_do:
+    added_results += eval(instruction)
+print(added_results)
+
